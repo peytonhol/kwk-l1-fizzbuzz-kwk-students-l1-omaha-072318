00e78475c853f1f3bd8680ec84aba9eb32a17375
peytonhol <peyhol@yahoo.com>
2018-07-25 19:12:04 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -1,9 +1,18 @@
-counter = 0 
+# counter = 0 
 
-loop do
-  counter += 1 
-  puts "Iteration #{counter} of the loop"
-  if counter >= 10 
-    break
-  end
+# loop do
+#   counter += 1 
+#   puts "Iteration #{counter} of the loop"
+#   if counter >= 10 
+#     break
+#   end
+# end
+
+num_of_hotdogs_eaten = 0
+
+while num_of_hotdogs_eaten < 7
+  num_of_hotdogs_eaten += 1
+  puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
 end
+
+puts "You ate a total of #{num_of_hotdogs_eaten} hot dogs!"
