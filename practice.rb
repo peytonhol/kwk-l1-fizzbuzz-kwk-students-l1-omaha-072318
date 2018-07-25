1231fae857e446e5e6e1d47fdccf4d5b6d9cde1e
peytonhol <peyhol@yahoo.com>
2018-07-25 19:19:41 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -1,23 +1,22 @@
-# # counter = 0 
+counter = 0 
 
-# # loop do
-# #   counter += 1 
-# #   puts "Iteration #{counter} of the loop"
-# #   if counter >= 10  
-# break
-# #   end
-# # end
+loop do
+  counter += 1 
+  puts "Iteration #{counter} of the loop"
+  if counter >= 10  
+  break
+  end
+end
 
-# num_of_hotdogs_eaten = 0
+num_of_hotdogs_eaten = 0
 
-# while num_of_hotdogs_eaten < 7
-#   num_of_hotdogs_eaten += 1
-#   puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
-# end
+while num_of_hotdogs_eaten < 7
+  num_of_hotdogs_eaten += 1
+  puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
+end
 
 counter = 0 
 until counter == 20
   puts "The current number is less than 20"
   counter += 1
 end 
-# puts "You ate a total of #{num_of_hotdogs_eaten} hot dogs!"
