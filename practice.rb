22dd0dd4b0a6d646288da6d7d2bde45342441c22
peytonhol <peyhol@yahoo.com>
2018-07-25 19:14:36 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -10,7 +10,7 @@
 
 num_of_hotdogs_eaten = 0
 
-while num_of_hotdogs_eaten < 54
+while num_of_hotdogs_eaten < 108
   num_of_hotdogs_eaten += 2
   puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
 end
